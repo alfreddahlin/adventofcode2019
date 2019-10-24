0bf044c8ba05a1cf680d21f8c6744a3fa67b0b68
Alfred Dahlin <daahlin@hotmail.com>
2019-10-24 16:55:37 +0200

    Added template and shell script for getting input.

---
+++ start_solving.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+DAY=$1
+
+URL='https://adventofcode.com/2019/day/'$DAY
+INPUT_FILE=inputs/day$DAY.in
+SOLUTION_FILE=day$DAY.go
+
+max_fails=10
+cur_fails=0
+until $(curl $URL'/input' --config aoc_session --output $INPUT_FILE --silent --fail --retry 10 --retry-delay 5)
+do
+    ((cur_fails++))
+    echo $cur_fails': Not quite yet...'
+    if [ $cur_fails -ge $max_fails ]
+    then
+       echo 'Puzzle not yet released, please be patient.'
+       exit 1
+    fi
+    sleep 5
+done
+
+echo "It's ready, start solving!"
+cp -i day0.go $SOLUTION_FILE
+google-chrome $URL
+emacs $INPUT_FILE $SOLUTION_FILE &
